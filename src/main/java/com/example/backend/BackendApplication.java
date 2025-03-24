d901aee2cb8871cac1d81daccd446724d5a097b3
“sungjunkim” <kis07150@naver.com>
2025-03-24 14:03:13 +0900

    test

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
+//	test
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
